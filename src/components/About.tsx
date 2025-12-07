1b3b7e369d594d2dd1e6a674c3dc8b455b904a23
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-12-07 08:17:20 +0600

    have i cooked

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -156,7 +156,7 @@ const About = () => {
                     rel="noopener noreferrer"
                     className="syntax-string hover:underline"
                   >
-                    "View My Resume"
+                    "View My Resume<span className="text-xl">↗</span>" 
                   </a>
                 </div>
               </div>
